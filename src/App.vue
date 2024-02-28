5af3bd001f00d8a7688752f641fde8bf2587e7b6
Amélie <amelie.adele@sfr.fr>
2024-02-28 13:06:13 +0100

    Ajout du contenu du component About avec ajout des polices et du fond

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script >
 
 </script>
 
@@ -7,6 +7,30 @@
   <router-view></router-view>
 </template>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Allura&family=Lato:ital,wght@0,400;0,700;1,400&display=swap');
+
+@font-face {
+  font-family: "Nickainley";
+  src: url('../src/assets/fonts/Nickainley-Normal.ttf.woff')
+}
+
+#app{
+  font-family: "Nickainley";
+  /*color : #004aad;*/
+  padding: 2em;
+  background-image: url(../src/assets/images/fond.png);
+  background-size: cover;
+}
+
+.title {
+  font-family: "Allura", cursive;
+  font-weight: 800;
+  font-style: normal;
+  font-size: 2.5em;
+}
+
+
+
 
 </style>
