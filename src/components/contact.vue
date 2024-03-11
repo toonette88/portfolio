ca069a53d82e50d66357881e7d82af175f39646f
Amélie <amelie.adele@sfr.fr>
2024-03-11 19:53:32 +0100

    Test d'envoi de mail

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,99 +1,112 @@
 <template>
-  <h1 class="title">Contact</h1>
-  <form ref="form" @submit.prevent="sendEmail">
-    <div class="name">
-      <div class="form_items name">
-        <label for="last_name">Nom :</label>
-        <input
-          type="text"
-          v-model="last_name"
-          name="last_name"
-          id="last_name"
-        />
+    <h1 class="title">Contact</h1>
+    <form ref="form" @submit.prevent="sendEmail()">
+      <div class="name">
+        <div class="form_items name">
+          <label for="last_name">Nom :</label>
+          <input
+            type="text"
+            v-model="last_name"
+            name="last_name"
+            id="last_name"
+          />
+        </div>
+        <div class="form_items name">
+          <label for="first_name">Prénom : </label>
+          <input
+            type="text"
+            vmodel="first_name"
+            name="first_name"
+            id="first_name"
+          />
+        </div>
       </div>
-      <div class="form_items name">
-        <label for="first_name">Prénom : </label>
-        <input
-          type="text"
-          vmodel="fisrt_name"
-          name="first_name"
-          id="first_name"
-        />
+      <div class="form_items">
+        <label for="object">Objet : </label>
+        <input type="text" v-model="object" name="object" id="object" />
       </div>
-    </div>
-    <div class="form_items">
-      <label for="object">Objet : </label>
-      <input type="text" v-model="object" name="object" id="object" />
-    </div>
-    <div class="form_items">
-      <label for="message" id="message_title">Message : </label>
-      <textarea v-model="message" name="message" id="message"></textarea>
-    </div>
-    <input type="submit" value="Envoyer" id="submit_button" />
-  </form>
-</template>
-
-<style scoped>
-form {
-  display: flex;
-  flex-direction: column;
-  width: 60%;
-  min-width: 600px;
-  margin-bottom: 10px;
-}
-
-.name {
-  display: flex;
-  flex-direction: row;
-}
-
-.form_items {
-  display: flex;
-  font-size: 2em;
-  padding: 5px;
-  margin: 5px;
-  align-items: center;
-}
-
-#submit_button {
-  width: 150px;
-  align-self: center;
-}
-
-input {
-  margin-left: 10px;
-  width: 150px;
-  background-color: cadetblue;
-  color: aliceblue;
-}
-
-label {
-  margin-left: 45px;
-}
-
-#message_title {
-  margin-left: 0px;
-}
-
-textarea {
-  width: 70%;
-  min-height: 100px;
-  margin-left: 10px;
-  background-color: cadetblue;
-  color: aliceblue;
-}
-</style>
-
-<script setup>
-function sendEmail() {
-  if (!last_name.value || !first_name.value | !object.value || !message.value) {
-    alert("Tous les champs doivent être complétés");
-  } else {
-    last_name.value = null;
-    first_name.value = null;
-    object.value = null;
-    message.value = null;
-    alert("Votre messagea été envoyé");
+      <div class="form_items">
+        <label for="message" id="message_title">Message : </label>
+        <textarea v-model="message" name="message" id="message"></textarea>
+      </div>
+      <input type="submit" value="Envoyer" id="submit_button" />
+    </form>
+  </template>
+  
+  <style scoped>
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+    min-width: 600px;
+    margin-bottom: 10px;
   }
-}
-</script>
+  
+  .name {
+    display: flex;
+    flex-direction: row;
+  }
+  
+  .form_items {
+    display: flex;
+    font-size: 2em;
+    padding: 5px;
+    margin: 5px;
+    align-items: center;
+  }
+  
+  #submit_button {
+    width: 150px;
+    align-self: center;
+  }
+  
+  input {
+    margin-left: 10px;
+    width: 150px;
+    background-color: cadetblue;
+    color: aliceblue;
+  }
+  
+  label {
+    margin-left: 45px;
+  }
+  
+  #message_title {
+    margin-left: 0px;
+  }
+  
+  textarea {
+    width: 70%;
+    min-height: 100px;
+    margin-left: 10px;
+    background-color: cadetblue;
+    color: aliceblue;
+  }
+  </style>
+  
+  <script setup>
+  
+  import {ref} from 'vue'
+  const last_name = ref("");
+  const first_name = ref("");
+  const object = ref("");
+  const message = ref("");
+  
+  function sendEmail() {
+    (function(){
+  
+      emailjs.init({'en-GWb-fN6-HqSTv3m'})();
+    const params = {
+      to_name: "Amelie",
+      from_name: last_name.value,
+      about: object.value,
+      message: message.value
+    }
+         
+        emailjs.send(service_msjzgvm, template_6flcsrcD, params)
+        .then( res => {
+          alert("E-mail envoyé avec succès");
+        })
+        .catch();
+      })}
+  </script>
---
+++ src/components/navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <router-link to="/">Accueil</router-link> | 
+        <router-link to="/">Autre page</router-link>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
